f8f8fdaaaf9e4c30fe9af47ce911ddc8b32c410b
yjchoco <yjchoco@github.com>
2025-07-30 06:22:56 +0000

    condtsf changes

--- CondTSF/distill_dlinear_condtsf.py
+++ CondTSF/distill_dlinear_condtsf.py
@@ -111,6 +111,8 @@ class Traj_Matching:
             target_params = torch.cat([p.data.to(self.device).reshape(-1) for p in target_params], 0)
             student_params = torch.cat([p.data.to(self.device).reshape(-1) for p in starting_params], 0).requires_grad_(True)
             start_params = torch.cat([p.data.to(self.device).reshape(-1) for p in starting_params], 0)
+            plugin_params = expert_traj[-1]
+            plugin_params = torch.cat([p.data.to(self.device).reshape(-1) for p in plugin_params], 0)
                       
             for _ in range(args.syn_steps):
                 # print(synx.shape())
@@ -120,12 +122,6 @@ class Traj_Matching:
                 grad = torch.autograd.grad(loss_syn, student_params, create_graph=True, allow_unused=True)[0]
                 student_params = student_params - syn_lr * grad
 
-
-            # CondTSF plugin
-            if i % 3 == 0:  
-                target_Y = student_net(synx, flat_param=target_params).squeeze().detach()
-                syny.data = (1 - args.beta) * syny.data + args.beta * target_Y
-
             param_loss = torch.nn.functional.mse_loss(student_params, target_params, reduction="sum")
             param_dist = torch.nn.functional.mse_loss(start_params, target_params, reduction="sum")
 
@@ -133,14 +129,18 @@ class Traj_Matching:
             param_loss = param_loss / num_params
             grand_loss = param_loss / param_dist
 
+            # CondTSF plugin
+            if i % 3 == 0:  
+                target_Y = student_net(synx, flat_param=plugin_params).squeeze().detach()
+                syny.data = (1 - args.beta) * syny.data + args.beta * target_Y
 
-            optimizer.zero_grad()
-            optimizer_lr.zero_grad()
-            grand_loss.backward()
-            optimizer.step()
-            optimizer_lr.step()
-
-            args.lr_teacher = syn_lr
+            else:
+                optimizer.zero_grad()
+                optimizer_lr.zero_grad()
+                grand_loss.backward()
+                optimizer.step()
+                optimizer_lr.step()
+                args.lr_teacher = syn_lr
             
             print(f"epoch:{i}, train loss: {grand_loss.item()}")
             if (i+1)%5== 0:
@@ -159,20 +159,15 @@ class Traj_Matching:
                     torch.save({'x':synx_, 'y':syny_}, args.save_path + ".pt")
 
 
-# python -m MTT.traj_matching_dlinear_condtsf -de 3 -e 1000 -d '../data/GBA' --params '../data/params/GBA-DLinear/' -sp results/tm_gba_condtsf.txt -lrf 1e-2 -lrs 1e-3 -sr 0.01
-# python -m MTT.traj_matching_dlinear_condtsf -de 3 -e 1000 -d '../data/GLA' --params '../data/params/GLA-DLinear/' -sp results/tm_gla_condtsf.txt -lrf 1e-2 -lrs 1e-3 -sr 0.01
-# python -m MTT.traj_matching_dlinear_condtsf -de 3 -e 1000 -d '../data/CA' --params '../data/params/CA-DLinear/' -sp results/tm_ca_condtsf.txt -lrf 1e-2 -lrs 1e-3 -sr 0.01
-# python -m MTT.traj_matching_dlinear_condtsf -de 3 -e 1000 -d '../data/ERA5' --params '../data/params/ERA5-DLinear/' -sp results/tm_era5_condtsf.txt -lrf 1e-2 -lrs 1e-3 -sr 0.01
-
-# python -m CondTSF.distill_dlinear_condtsf -de 0 -e 300 -d '../data/AURORA_final' --params '../data/params/AURORA-Dlinear/' -sp results/condtsf_aurora -sr 0.005 -lrf 1e-2 -lrs 1e-3 
-
+# python -m CondTSF.distill_dlinear_condtsf -e 100 -d '../data/AURORA_final' --params '../data/params/AURORA-Dlinear/' -sr 0.005 -lrf 1e-3 -lrs 1e-2 -sp results/condtsf_aurora_0 -de 0 -s 0
+# python -m CondTSF.distill_dlinear_condtsf -e 300 -d '../data/GBA' --params '../data/params/GBA-Dlinear/' -sr 0.005 -lrf 1e-3 -lrs 1e-2 -sp results/condtsf_aurora_3_2 -de 5 -s 0
 
 if __name__ == "__main__":
     torch.set_num_threads(4)
     parser = argparse.ArgumentParser()
     parser.add_argument('-de', '--device', type=int, default=0, help='')
     parser.add_argument('-d', '--data', type=str, default='../data/METR-LA', help='data path')
-    parser.add_argument('-b', '--batch_size', type=int, default=48, help='batch sizefor real data')#128
+    parser.add_argument('-b', '--batch_size', type=int, default=64, help='batch sizefor real data')#128
     parser.add_argument('-lrs', '--lr_syn',type=float,default=1e-3,help='learning rate for testing on synthetic data')
     parser.add_argument('-lrf', '--lr_feat',type=float,default=0.1,help='learning rate for updating synthetic data')
     parser.add_argument('-sr', '--series_reduce_rate',type=float,default=2e-2,help='learning rate')
