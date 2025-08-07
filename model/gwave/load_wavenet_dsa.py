060ce09a030b311ddd7e343c9a48ac64aacb027b
kbrother <00076289@kaist.ac.kr>
2025-08-07 13:53:22 +0000

    save model

--- model/gwave/load_wavenet_dsa.py
+++ model/gwave/load_wavenet_dsa.py
@@ -61,7 +61,7 @@ def test_gwnet(args, data, raw_data, device):
             optimizer.step()
 
         _model.eval()
-        if (i+1)%10 == 0:
+        if (i+1)%5 == 0:
             with torch.no_grad():
                 _model.embed_forward(nm_input_real)
                 val_loss = math.sqrt(_model.test_model(data['val_loader'], scaler, device))
@@ -79,9 +79,12 @@ def test_gwnet(args, data, raw_data, device):
         test_loss = math.sqrt(_model.test_model(data['test_loader'], scaler, device))
 
     print(f"min i: {min_i}, val loss: {min_val_loss}, test loss: {test_loss}")
+    with open(args.save_path, "a") as f:
+        f.write(f"min i: {min_i}, val loss: {min_val_loss}, test loss: {test_loss}\n")
 
 
-# python -m model.gwave.load_wavenet_dsa -de 0 -d ../data/GBA -lrs 1e-3 -e 400 -lp results_ours/dc_dsa_cluster_gba_1e-3_1e-3.pt -s 0
+# python -m model.gwave.load_wavenet_dsa -de 0 -d ../data/GBA -lrs 1e-2 -e 400 -lp results/dc_dsa_cluster_gba_1e-3_1e-3.pt -s 0
+# python -m model.gwave.load_wavenet_dsa -de 0 -d ../data/GLA -lrs 1e-2 -e 400 -lp results/dc_dsa_cluster_gla_1e-3_1e-2_2.pt -s 0
 # python -m model.gwave.load_wavenet -de 7 -d ../data/GLA -lrs 1e-3 -lp results/dc_clus_gla.pt
 # python -m model.gwave.load_wavenet -de 7 -d ../data/ERA5 -lrs 1e-3 -lp results/dc_clus_era5.pt
 if __name__ == "__main__":
@@ -97,6 +100,7 @@ if __name__ == "__main__":
     parser.add_argument('-nh', '--nhid', type=int, default=32, help='')
     parser.add_argument('-dr', '--dropout',type=float,default=0.3,help='dropout rate')
     parser.add_argument('-ned', '--ne_dim',type=int,default=32,help='')
+    parser.add_argument('-sp', '--save_path', type=str, default='results/')
     args = parser.parse_args()
 
     # random seed setting
